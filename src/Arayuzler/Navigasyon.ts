f41fa0cd87fd549cd8304adb5a1923ba55273ad2
Fatih APAYDIN <fatihapaydin@windowslive.com>
2017-08-17 12:09:41 +0300

    navigasyon arayüzü

--- src/Arayuzler/Navigasyon.ts
+++ src/Arayuzler/Navigasyon.ts
@@ -2,7 +2,7 @@ import { NavigationScreenProps } from 'react-navigation';
 
 export interface INavigasyon {
     id: number;
-    name: string;
+    name: string; ts
 }
 
 export interface IBaslangicProps extends NavigationScreenProps<INavigasyon> {
